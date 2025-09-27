81b4ee5909a2834a6f3dcaea7f7fef093d031eaa
KayaPapaya <kthorsen2004@gmail.com>
2025-09-27 16:14:08 +0200

    Task 2: - Implemented a "Car" class with the properties "LicensePlate" and "Date", as well as the methods "Price()" and "VehicleType().
    - Added XML-comments for "Car" class.
    
    - Added a testproject to test the two properties and two methods.
    
    - The testing of the properties ensures that the properties are getting stored and returns a correct value.
    
    - The testing of the methods ensures that the methods are returning the correct return value which is defined in the method in the "Car" class.

---
+++ Bridge/Car.cs
@@ -0,0 +1,35 @@
+﻿namespace Car
+{
+    /// <summary>
+    /// This is a public car class.
+    /// </summary>
+    public class Car
+    {
+        /// <summary>
+        /// Property for the license plate of the car.
+        /// </summary>
+        public string LicensePlate { get; set; }
+        /// <summary>
+        /// Property for the date.
+        /// </summary>
+        public DateTime Date { get; set; }
+
+        /// <summary>
+        /// A method that returns the price for a car to pass the bridge.
+        /// </summary>
+        /// <returns>The price for a car to pass <see cref="double"/>.</returns>
+        public double Price()
+        {
+            return 230;
+        }
+
+        /// <summary>
+        /// A method that returns the type of the car.
+        /// </summary>
+        /// <returns>The type of car as the string "Car".</returns>
+        public string VehicleType()
+        {
+            return "Car";
+        }
+    }
+}
---
+++ TheBridgeTicketSystemTests/CarTests.cs
@@ -0,0 +1,67 @@
+﻿using Car;
+
+namespace TheBridgeTicketSystemTests
+{
+    // Test only for cars in the car class.
+    [TestClass]
+    public sealed class CarTests
+    {
+        // This testmethod checks if the license plate property saves and returns.
+        [TestMethod]
+        public void LicensePlate_ShouldSaveAndReturnValue()
+        {
+            // Arrange
+            var car = new Car.Car();
+            var lPlate = "AB12345";
+
+            // Act
+            car.LicensePlate = lPlate;
+
+            // Assert
+            Assert.AreEqual(lPlate, car.LicensePlate);
+        }
+
+        // This testmethod checks if the date property saves and returns.
+        [TestMethod]
+        public void Date_ShouldSaveAndReturnValue()
+        {
+            // Arrange
+            var car = new Car.Car();
+            var dDate = new DateTime(2025, 10, 4);
+
+            // Act
+            car.Date = dDate;
+
+            // Assert
+            Assert.AreEqual(dDate, car.Date);
+        }
+
+        // This testmethod checks if the price method returns 230.
+        [TestMethod]
+        public void CarPrice_ShouldReturn230()
+        {
+            // Arrange
+            var car = new Car.Car();
+
+            // Act
+            var price = car.Price();
+
+            // Assert
+            Assert.AreEqual(230, price);    
+        }
+
+        // This testmethod checks if the vehicle type method returns "Car".
+        [TestMethod]
+        public void VehicleType_ShouldReturnCar()
+        {
+            // Arrange
+            var car = new Car.Car();
+
+            // Act
+            var price = car.VehicleType();
+
+            // Assert
+            Assert.AreEqual("Car", price);
+        }
+    }
+}
