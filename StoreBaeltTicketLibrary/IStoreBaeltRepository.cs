706a03f65b45fa13a3c338cba955f9a82612dcae
KayaPapaya <kthorsen2004@gmail.com>
2025-10-01 11:00:00 +0200

    Task 11: - A repository class "StoreBaeltRepository" is added to the "StoreBaeltTicketLibrary". It contains a static list for all the tickets at "Storebaelt".
    - The method "AddTicket()"makes it possible to add a new ticket through the repository.
    
    - The method "GetAllTickets()" makes it possible to see all tickets through the repository.
    
    - The method "GetTicketsByLicense()" makes it possible to see all tickets for a specifik licenseplate through the repository.
    
    - There's also added an interface, where these three methods are also stored.
    
    - No tests are made for this task, since it's not asked for (required). But it could be interesting to see if these methods would work :)

---
+++ StoreBaeltTicketLibrary/IStoreBaeltRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VehicleBase;
+
+namespace StoreBaeltTicketLibrary
+{
+    public interface IStoreBaeltRepository
+    {
+        public interface IStoreBaeltRepository
+        {
+            void AddTicket(Vehicle vehicle);
+            List<Vehicle> GetAllTickets();
+            List<Vehicle> GetTicketsByLicense(string licensePlate);
+        }
+    }
+}
---
+++ StoreBaeltTicketLibrary/StoreBaeltRepository.cs
@@ -0,0 +1,27 @@
+﻿using VehicleBase;
+
+namespace StoreBaeltTicketLibrary
+{
+    public class StoreBaeltRepository
+    {
+        private static List<Vehicle> _tickets = new List<Vehicle>();
+
+        public void AddTicket(Vehicle vehicle)
+        {
+            _tickets.Add(vehicle);
+        }
+
+        public List<Vehicle> GetAllTickets()
+        {
+            return new List<Vehicle>(_tickets);
+        }
+
+        public List<Vehicle> GetTicketsByLicense(string licensePlate)
+        {
+            return _tickets
+                .Where(v => v.LicensePlate == licensePlate)
+                .ToList();
+        }
+    }
+}
+
